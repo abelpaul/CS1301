bab51299f6f65be7573f6f3fd2c7c4242010038d
Abel Paul George <abel.paul.george@gmail.com>
2017-10-17 10:06:23 -0400

    Classwork Oct 17

---
+++ FoorLoopClasswork.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ForLoopClasswork {
+  public static void main (String[] args) {
+    for (int i=0;i<8;i++) {
+      
+      System.out.println(i);
+      
+    }
+     for (int i=8;i>-1;i--) {
+      
+      System.out.println(i);
+      
+    }
+  }
+}
