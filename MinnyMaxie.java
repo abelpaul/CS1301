af3306e90b4f267545b67715dc71a1a00a9ef7aa
Abel Paul George <abelpaulgeorge@gmail.com>
2017-10-19 07:02:12 -0400

    october 17th

---
+++ MinnyMaxie.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MinnyMaxie {
+  public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
+    int num_1 = input.nextInt();
+    int num_2 = input.nextInt();
+    int num_3 = input.nextInt();
+    int num_4 = input.nextInt();
+    int num_5 = input.nextInt();
+    int num_6 = input.nextInt();
+    int num_7 = input.nextInt();
+    int num_8 = input.nextInt();
+    int num_9 = input.nextInt();
+    int num_10 = input.7nextInt();
+    for (int i =  
+  }
+}
