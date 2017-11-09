19f7612c54791f5e8178810a2d3c74c7013743e5
Abel Paul George <abelpaulgeorge@gmail.com>
2017-11-09 17:38:29 -0500

    docs: update firstname.java

---
+++ firstname.java
@@ -0,0 +1,14 @@
+public class firstname {
+  
+  public static void main(String[] args) {
+    String firstName = "Harry_Potter";
+    System.out.println(firstName.toUpperCase());
+    System.out.println(firstName); //uppercase makes string containing uppercase letters but does not change valeu of first name
+    
+    String test = "hello";
+    System.out.println(test.charAt(0));
+    System.out.println(test.charAt(3));
+    System.out.println(test.charAt(4)); // e would be atChar(1);
+  }
+}}
+    
