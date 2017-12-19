61b9ee2f157ca4cc9d111bfb8eddd75564ef0d5f
Abel Paul George <abelpaulgeorge@gmail.com>
2017-12-19 12:02:55 -0500

    docs : final update

---
+++ Strings.java
@@ -0,0 +1,15 @@
+public class Strings {
+  
+public static void main (String[] args)
+{
+  String str1 = "Frodo Baggins";  
+String str2 = "Gandalf the GRAY";  
+System.out.println(str1.length() );
+System.out.println(str1.indexOf("o")  );
+System.out.println(str2.toUpperCase() );
+System.out.println(str1.toLowerCase() );
+System.out.println(str1.indexOf("B") );
+System.out.println(str1.substring(5)  );
+System.out.println(str2.substring(3,14)  );
+}
+}
